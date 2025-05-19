152e2f3e3dc3fa7d6dd9a1f5730a23685c81479c
MaxI240 <millanaggarwal8@gmail.com>
2025-05-20 00:04:39 +0530

    Update untoldcoding.js

--- untoldcoding.js
+++ untoldcoding.js
@@ -1,21 +1,19 @@
 let currentPage = 1;
 
 function toggleClass(e, toggleClassName) {
-  if (e.className.includes(toggleClassName)) {
-    e.className = e.className.replace(" " + toggleClassName, "");
+  if (e.classList.contains(toggleClassName)) {
+    e.classList.remove(toggleClassName);
   } else {
-    e.className += " " + toggleClassName;
+    e.classList.add(toggleClassName);
   }
 }
 
 function movePage(e, page) {
-  if (page == currentPage) {
-    currentPage += 2;
+  if (page === currentPage) {
     toggleClass(e, "left-side");
-    toggleClass(e.nextElementSibling, "left-side");
-  } else if ((page = currentPage - 1)) {
-    currentPage -= 2;
+    currentPage++;
+  } else if (page === currentPage - 1) {
     toggleClass(e, "left-side");
-    toggleClass(e.previousElementSibling, "left-side");
+    currentPage--;
   }
 }
